d392fdf7440bbe8bc1ca386f7daac4f9b5c7a12e
Brenda Marmolejo <bmarmolejo@gmail.com>
2024-07-13 22:47:02 -0400

    updated pictures to fix deployment issues

--- enobot-client/src/components/OpenAIEnobot/OpenAIEnobot.jsx
+++ enobot-client/src/components/OpenAIEnobot/OpenAIEnobot.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './OpenAIEnobot.scss';
+import enobotIcon from '../../assets/images/enobot-black.svg';
 
 const HTTP_ENDPOINT = "http://localhost:8080/api/predict"; 
 
@@ -36,7 +37,7 @@ const OpenAIEnobot = ({ type, variety, region, onResponse }) => {
   return (
     <div className="openai-enobot">
       <div className="openai-enobot__header">
-        <img src="/src/assets/images/enobot-black.svg" alt="EnoBot Icon" className="enobot-icon" />
+        <img src={enobotIcon} alt="EnoBot Icon" className="enobot-icon" />
         <span>Hello there! I'm EnoBot, your personal wine sommelier. Let's talk wine! </span>
       </div>
       <div className="openai-enobot__body">
